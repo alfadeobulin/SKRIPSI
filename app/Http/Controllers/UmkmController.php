c485239470a4f448e3468729f521c1137d8ff644
alfadeobulin <alfadeojeremy@gmail.com>
2021-06-25 17:19:02 +0700

    Validasi 80%

--- app/Http/Controllers/UmkmController.php
+++ app/Http/Controllers/UmkmController.php
@@ -22,17 +22,44 @@ class UmkmController extends Controller
       return view ('umkm/usaha', ['usaha' => $usaha]);
   }
 
-  public function delete($id_usaha)
-  {
-    DB::table('usaha')->where('id_usaha', $id_usaha)->delete();
-    return redirect('/umkm')->with('sukses','Data berhasil dihapus!');
-  }
-
   public function create(Request $request)
     {
+      $this->validate($request,
+      [
+          'id_usaha' => 'required|min:4|max:10|unique:usaha',
+          'nama_usaha' => 'required',
+          'alamat_ush' => 'required',
+          'ket_ush' => 'required',
+          'longitude' => 'required',
+          'latitude' => 'required',
+          'id_member' => 'required|min:4|max:10|unique:member',
+          'id_usaha' => 'required|min:3|max:10',
+          'id_kel' => 'required|min:4|max:10|unique:kelurahan',
+          'id_kec' => 'required|min:4|max:10|unique:kecamatan',
+          'ktrgn_foto' => 'required',
+      ],
+      [
+          'id_galeri.required' => 'ID galeri wajib di isi',
+          'id_galeri.min'      => 'ID galeri minimal 3 karakter',
+          'id_galeri.max' => 'ID galeri maksimal 3 digit',
+          'id_galeri.unique' => 'ID galeri Sudah Digunakan',
+          'nama_galeri.required'   => 'Nama wajib di isi',
+          'foto.required' => 'Foto wajib di unggah',
+          'id_usaha.required' => 'ID usaha wajib di isi',
+          'id_usaha.min'      => 'ID usaha minimal 3 karakter',
+          'id_usaha.max' => 'ID usaha maksimal 3 digit',
+          'id_kel.required' => 'ID kelurahan wajib di isi',
+          'id_kel.min'      => 'ID kelurahan minimal 3 karakter',
+          'id_kel.max' => 'ID kelurahan maksimal 3 digit',
+          'id_kec.required' => 'ID kecamatan wajib di isi',
+          'id_kec.min'      => 'ID kecamatan minimal 3 karakter',
+          'id_kec.max' => 'ID kecamatan maksimal 3 digit',
+          
+      ]);
+
       //dd($request->all());
       $usaha = new Umkm;
-      $usaha->id_usaha = Auth::user()->id_usaha;
+      $usaha->id_usaha = $request->id_usaha;
       $usaha->nama_ush = $request->nama_ush;
       $usaha->alamat_ush = $request->alamat_ush;
       $usaha->ket_ush = $request->ket_ush;
@@ -42,6 +69,14 @@ class UmkmController extends Controller
       $usaha->id_kel = $request->id_kel;
       $usaha->id_kec = $request->id_kec;
       $usaha->save();
-      return redirect('/umkm/usaha')->with('sukses','Data berita berhasil ditambahkan');
+      return redirect('/umkm')->with('sukses','Data berita berhasil ditambahkan');
     }
+
+  public function delete($id_usaha)
+  {
+    DB::table('usaha')->where('id_usaha', $id_usaha)->delete();
+    return redirect('/umkm')->with('sukses','Data berhasil dihapus!');
+  }
+
+  
 }   
